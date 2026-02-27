57f4b1f7fa35e2d9bdef34f0f79def3f647c4ae8
KS <KS>
2026-02-27 17:10:13 +0530

    final

--- src/product/product.controller.ts
+++ src/product/product.controller.ts
@@ -1,15 +1,39 @@
-import { Controller, Get, Inject } from '@nestjs/common';
+import { Controller, Get, Post, Body, Param, Put, Delete, Inject } from '@nestjs/common';
 import { ClientProxy } from '@nestjs/microservices';
 
 @Controller('product')
 export class ProductController {
-    constructor(
-        @Inject('PRODUCT_SERVICE')
-        private client: ClientProxy,
-      ) {}
-    
-      @Get()
-      getUsers() {
-        return this.client.send('get_users', {});
-      }
+
+  constructor(
+    @Inject('PRODUCT_SERVICE')
+    private client: ClientProxy,
+  ) {}
+
+  @Get()
+  getAll() {
+    return this.client.send('findAll_products', {});
+  }
+
+  @Post('create_product')
+  create(@Body() body: any) {
+    return this.client.send('create_product', body);
+  }
+
+  @Get(':id')
+  getOne(@Param('id') id: string) {
+    return this.client.send('findOne_product', id);
+  }
+
+  @Post('update/:id')
+  update(@Param('id') id: string, @Body() body: any) {
+    return this.client.send('update_product', {
+      id,
+      ...body,
+    });
+  }
+
+  @Delete(':id')
+  remove(@Param('id') id: string) {
+    return this.client.send('remove_product', id);
+  }
 }
